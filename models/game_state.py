9e73daeaa0a143e8c94979ab1536fc33dcbf9ae5
Nicholas Rios <rios.nicholas@protonmail.com>
2023-08-29 12:30:18 -0400

    Create boilerplate code

--- models/game_state.py
+++ models/game_state.py
@@ -1,3 +1,19 @@
 #!/usr/bin/env python3
-class GameState:
-    pass
+from enum import Enum
+
+class GameState(Enum):
+    DRAW = 0
+    P1_WON = 1
+    P2_WON = 2
+    ONGOING = 3
+    # INVALID = 4
+
+    def check_if_won():
+        pass
+
+    def check_if_draw():
+        pass
+
+    def check_if_ongoing():
+        pass
+
