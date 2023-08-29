76431956d0e9ba2166b187b3d84a1e86a199c3be
Nicholas Rios <rios.nicholas@protonmail.com>
2023-08-28 20:48:23 -0400

    Remove unused files

--- tic_tac_toe.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-"""
-Receives user input, i.e., where a user token (‘X’ or ‘O’) has been put
-Returns the state of the game as a result:
-  did any of the players win, is it a draw, or is the game still ongoing?
-"""
-
