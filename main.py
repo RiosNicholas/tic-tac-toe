e8cc78432ec261daaf9ae587af0172e416c209d5
Nicholas Rios <rios.nicholas@protonmail.com>
2023-08-29 14:43:14 -0400

    Create token selection method

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
-#!/usr/bin/env python3
+from game_state import GameState
+from tic_tac_toe import TicTacToe
+
+print("Welcome to Tic-Tac-Toe...")
+
 """
-Receives user input, i.e., where a user token (‘X’ or ‘O’) has been put
-Returns the state of the game as a result:
-  did any of the players win, is it a draw, or is the game still ongoing?
+TODO: Summary goes here
+
+Returns:
+  Did any of the players win, is it a draw, or is the game still ongoing?
 """
+def main():
+  # RECEIVES USER INPUT TO SELECT THEIR TOKEN (‘X’ OR ‘O’), REMOVING SURROUNDING WHITE SPACE AND CONVERTING THE STRING TO UPPERCASE.
+  selected_token = input("Select your token (X or O):").strip().upper()
+
+  if selected_token == 'X' or selected_token == 'O':
+      game = TicTacToe(current_player=selected_token)
+  else:
+      print("Invalid input. Defaulting to 'X'.")
+      game = TicTacToe(current_player='X')
